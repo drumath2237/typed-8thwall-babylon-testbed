f9663e3517b73eb47eeabe1f10cdf38abbd4e3a0
drumath <2237k.drumath@gmail.com>
2023-05-09 02:26:56 +0900

    :sparkles: babylonでcubeを出すまで

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import './style.scss';
+import { Engine, MeshBuilder, Scene, Vector3 } from '@babylonjs/core';
 
 const main = () => {
   const renderCanvas = document.getElementById(
@@ -11,7 +12,20 @@ const main = () => {
   renderCanvas.width = document.firstElementChild?.clientWidth ?? 0;
   renderCanvas.height = document.firstElementChild?.clientHeight ?? 0;
 
-  console.log('Hello');
+  const engine = new Engine(renderCanvas, true);
+  const scene = new Scene(engine);
+
+  scene.createDefaultCameraOrLight(true, true, true);
+
+  MeshBuilder.CreateBox('box', { size: 0.1 }).position = new Vector3(
+    0,
+    0.05,
+    0
+  );
+
+  engine.runRenderLoop(() => {
+    scene.render();
+  });
 };
 
 main();

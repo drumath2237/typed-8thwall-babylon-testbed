f8ebff96788612e0d9e365ec1c9c03c9e9e65ece
drumath2237 <2237k.drumath@gmail.com>
2023-05-12 00:35:26 +0900

    :sparkles: mkcert pluginによってhttps化を実現できた

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # typed-8thwall-babylon-testbed
 
-```
-openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout key.pem -out cert.pem
-```
-
 ```env
 # .env
 VITE_API_KEY=xxxxxxxxxxxxxxx
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite';
-import * as fs from 'fs';
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig(({ mode }) => {
   if (mode === 'development') {
     return {
       server: {
-        https: {
-          key: fs.readFileSync('./key.pem'),
-          cert: fs.readFileSync('./cert.pem'),
-        },
+        https: true,
         port: 3000,
       },
+      plugins: [mkcert()],
     };
   }
   return {};
